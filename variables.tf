41f5b21d93fcfb0bb0a3eb22824352c438fa20c0
kiran-ab01 <132429361+kiran-ab01@users.noreply.github.com>
2025-11-20 12:42:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,26 @@
-variable "ecr_repo_name" {
+variable "env" {
   type        = string
-  description = "Existing ECR repository name"
+  description = "Environment name (uat/prod)"
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = "AWS CLI profile for the environment"
+}
+
+variable "ecs_task_role_arn" {
+  type        = string
+  description = "Existing IAM Role ARN for ECS task execution"
+}
+
+variable "image_uri" {
+  type        = string
+  description = "Full URI of the Docker image (including tag)"
 }
 
 variable "subnet_ids" {
@@ -11,4 +31,9 @@ variable "subnet_ids" {
 variable "security_group_ids" {
   type        = list(string)
   description = "Security Groups for ECS Tasks"
+}
+
+variable "desired_count" {
+  type    = number
+  default = 1
 }
